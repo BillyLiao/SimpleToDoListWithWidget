1b76f44c88b9ea9b23ae7b106ac213b9068645dd
廖慶麟 <hohey670@gmail.com>
2018-01-04 17:51:05 +0800

    First commit

---
+++ SimpleToDoList/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  SimpleToDoList
+//
+//  Created by 廖慶麟 on 2018/1/4.
+//  Copyright © 2018年 廖慶麟. All rights reserved.
+//
+
+import Foundation
+
+public struct ToDoItem {
+    
+}
